dba9073506ee76ee76c362f5dc94701cf22f59b4
Joel <joelruiz2@gmail.com>
2024-05-25 01:18:14 -0500

    added return for notifications/subscribe to avoid multiple headers

--- src/api/notifications.js
+++ src/api/notifications.js
@@ -1,42 +1,48 @@
 const { Router } = require("express");
 
-
-const PersonService = require('shared/services/person');
+const PersonService = require("shared/services/person");
 const { GeneralError } = require("shared/util/errorhandler");
 const person = new PersonService();
 
 module.exports = function routes(middleware) {
-
     let router = new Router();
 
-    router.post('/notifications/subscribe', middleware, async (req, res, next) => {
-        try {
-            const subscription = req.body
-            const user = req.user;
+    router.post(
+        "/notifications/subscribe",
+        middleware,
+        async (req, res, next) => {
+            try {
+                const subscription = req.body;
+                const user = req.user;
 
-            if (!user) {
-                throw new GeneralError('E_NOT_AUTHORIZED');
+                if (!user) {
+                    throw new GeneralError("E_NOT_AUTHORIZED");
+                }
+
+                let shortid = user.shortid;
+                //disable notifications
+                if (!subscription) {
+                    let results = await person.updateUser({
+                        shortid,
+                        webpush: null,
+                    });
+                    res.status(200).json({ success: true });
+                    return;
+                }
+
+                console.log(subscription);
+
+                let results = await person.updateUser({
+                    shortid,
+                    webpush: JSON.stringify(subscription),
+                });
+
+                res.status(200).json({ success: true });
+            } catch (e) {
+                next(e);
             }
-
-            let shortid = user.shortid;
-            //disable notifications
-            if (!subscription) {
-                let results = await person.updateUser({ shortid, webpush: null });
-                res.status(200).json({ 'success': true })
-            }
-
-            console.log(subscription)
-
-            let results = await person.updateUser({ shortid, webpush: JSON.stringify(subscription) });
-
-            res.status(200).json({ 'success': true })
         }
-        catch (e) {
-            next(e);
-        }
-
-    });
+    );
 
     return router;
-}
-
+};
