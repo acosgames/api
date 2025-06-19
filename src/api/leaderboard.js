f48c9f8747d25716ce323ca2a323bc6d94f1bd9a
Joel <joelruiz2@gmail.com>
2025-06-18 19:15:25 -0500

    finalizing leaderboard logic and fixing leaderboard filters

--- src/api/leaderboard.js
+++ src/api/leaderboard.js
@@ -28,26 +28,32 @@ module.exports = class LeaderboardAPI {
                 next();
             });
 
-        this.router.get(
-            "/api/v1/game/rankings/:game_slug/:countrycode",
+        this.router.post(
+            "/api/v1/game/leaderboard",
             middleware,
-            this.apiFindGameRankNational.bind(this)
+            this.apiFindLeaderboard.bind(this)
         );
-        this.router.get(
-            "/api/v1/game/rankings/:game_slug",
-            middleware,
-            this.apiFindGameRankGlobal.bind(this)
-        );
-        this.router.get(
-            "/api/v1/game/rankings/division/:game_slug/:division",
-            middleware,
-            this.apiFindGameRankDivision.bind(this)
-        );
-        this.router.get(
-            "/api/v1/game/lbhs/:game_slug",
-            middleware,
-            this.apiFindGameLeaderboardHighscore.bind(this)
-        );
+
+        // this.router.get(
+        //     "/api/v1/game/rankings/:game_slug/:countrycode",
+        //     middleware,
+        //     this.apiFindGameRankNational.bind(this)
+        // );
+        // this.router.get(
+        //     "/api/v1/game/rankings/:game_slug",
+        //     middleware,
+        //     this.apiFindGameRankGlobal.bind(this)
+        // );
+        // this.router.get(
+        //     "/api/v1/game/rankings/division/:game_slug/:division",
+        //     middleware,
+        //     this.apiFindGameRankDivision.bind(this)
+        // );
+        // this.router.get(
+        //     "/api/v1/game/lbhs/:game_slug",
+        //     middleware,
+        //     this.apiFindGameLeaderboardHighscore.bind(this)
+        // );
 
         return this.router;
     }
@@ -61,62 +67,92 @@ module.exports = class LeaderboardAPI {
 
         return this.actionRouter;
     }
+    async apiFindLeaderboard(req, res, next) {
+        let response = null;
+        try {
+            let user = req.user;
+            let displayname = user?.displayname || undefined;
+
+            let config = {
+                displayname,
+            };
+
+            config.limit = Number.parseInt(config.limit);
+            if (!config.limit) {
+                config.limit = 100;
+            }
+            config.limit = Math.min(100, Math.max(10, config.limit));
+
+            config = Object.assign({}, req.body, config);
+
+            response = await leaderboard.getLeaderboard(config);
+            res.json(response);
+        } catch (e) {
+            console.error(e);
+            next(e);
+        }
+    }
 
     async apiFindGameRankNational(req, res, next) {
-        let g = null;
+        let response = null;
         try {
             let user = req.user;
+            let displayname = user?.displayname || undefined;
             let game_slug = req.params.game_slug;
             let countrycode = req.params.countrycode;
-            g = await leaderboard.findGameRankNational(
-                game_slug,
-                user?.shortid,
-                user?.displayname,
-                countrycode
-            );
 
-            // redisRankings = await leaderboard.getRatingLeaderboardRedis({ game_slug, countrycode, season: 0 });
-            // console.log(redisRankings);
+            let config = {
+                type: "rank",
+                game_slug,
+                countrycode,
+                displayname,
+            };
+
+            response = await leaderboard.getLeaderboard(config);
+            res.json(response);
         } catch (e) {
             next(e);
-            return;
         }
-
-        res.json(g);
-        return;
     }
     async apiFindGameRankDivision(req, res, next) {
-        let g = null;
         try {
             let user = req.user;
+            let displayname = user?.displayname || undefined;
             let game_slug = req.params.game_slug;
             let division_id = req.params.division;
-            g = await leaderboard.findGameRankDivision(game_slug, division_id);
+
+            let config = {
+                type: "division",
+                game_slug,
+                division_id,
+                displayname,
+            };
+
+            let response = await leaderboard.getLeaderboard(config);
+
+            res.json(response);
         } catch (e) {
             next(e);
-            return;
         }
-
-        res.json(g);
-        return;
     }
     async apiFindGameRankGlobal(req, res, next) {
-        let g = null;
+        let response = null;
         try {
             let user = req.user;
+            let displayname = user?.displayname || undefined;
             let game_slug = req.params.game_slug;
-            g = await leaderboard.findGameRankGlobal(
+
+            let config = {
+                type: "rank",
                 game_slug,
-                user?.shortid,
-                user?.displayname
-            );
+                displayname,
+            };
+
+            response = await leaderboard.getLeaderboard(config);
+            res.json(response);
         } catch (e) {
             next(e);
-            return;
         }
-
-        res.json(g);
-        return;
     }
 
     async apiFindGameLeaderboardHighscore(req, res, next) {
